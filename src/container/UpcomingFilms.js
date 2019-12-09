61a73d4181074f38a9825c3db862216dabc1ec28
Alex Forrester <alexforrester96@gmail.com>
2019-12-09 16:35:50 +0000

    finished

---
+++ src/container/UpcomingFilms.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+class UpcomingFilms extends Component {
+
+  render() {
+
+    return(
+      <div className="UpcomingFilms">
+        <a href="https://www.imdb.com/calendar/?region=gb">
+        View Upcoming Films
+        </a>
+      </div>
+    )
+  }
+
+}
+
+export default UpcomingFilms;
